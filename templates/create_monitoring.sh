68d8f43584b24ff7000e11d1583f19dc5fe62bfd
Maycon Carlete <maycon.carlete@gmail.com>
2023-04-24 21:26:50 -0300

    fix: cookiecutter configuration

--- templates/create_monitoring.sh
+++ templates/create_monitoring.sh
@@ -2,12 +2,19 @@
 
 MONITORING_NAME=""
 SCHEDULE_INTERVAL=""
+START_DATE=""
 
+
+# get the data from using these questions
 read -p "Enter the monitoringg name: " MONITORING_NAME
 read -p "Enter the schedule interval: " SCHEDULE_INTERVAL
+read -p "Enter the start date: " START_DATE
 
-echo "logging: Monitoring name: ${MONITORING_NAME} Schedule Interval: ${SCHEDULE_INTERVAL}"
+# logging
+echo "logging: Monitoring name: ${MONITORING_NAME} Schedule Interval: ${SCHEDULE_INTERVAL} start date: ${START_DATE}"
 echo "logging: Creating monitoring..."
+
 MONITORING_PATH="/home/maycon/dev/airflow/airflow-submodule/templates"
 MONITORING_OUT_DIR="/home/maycon/dev/airflow/airflow-submodule/src/dags"
-echo $(python3 -m cookiecutter ${MONITORING_PATH} --no-input monitoring_name="${MONITORING_NAME}" schedule_interval="${SCHEDULE_INTERVAL}" -o ${MONITORING_OUT_DIR})
+
+echo $(python3 -m cookiecutter ${MONITORING_PATH} --no-input monitoring_name="${MONITORING_NAME}" schedule_interval="${SCHEDULE_INTERVAL} date" -o ${MONITORING_OUT_DIR})
