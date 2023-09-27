2a3a31687d3439dc35d55901f30ecfc7c6267a6f
Maycon Carlete <maycon.carlete@gmail.com>
2023-09-26 21:55:30 -0400

    refactor: adjust etl part

--- templates/{{cookiecutter.monitoring_name}}/etl/entrypoint.py
+++ templates/{{cookiecutter.monitoring_name}}/etl/entrypoint.py
@@ -7,23 +7,22 @@ from engine.common.utils import load_config_file_to_dict
 # Add support to run locally with Minio
 
 def entrypoint(**kwargs) -> dict[str, pd.DataFrame]:
-    configs = kwargs.get("configs")
-    print(f'Available resources: {configs}')
-    etl = configs.get("etl")
+    print(f'Available resources: {kwargs}')
+    etl = kwargs.get("etl")
     sqls = etl.get("sqls")
-    cursor = connect(
-        s3_staging_dir="s3://aws-athena-query-results-732179175681-us-west-2/",
-        region_name="us-west-2"
-    ).cursor()
+    s3_staging_dir = etl.get("s3_staging_dir")
+    region = etl.get("region")
+    cursor = connect(s3_staging_dir=s3_staging_dir, region_name=region).cursor()
     etl_results = {}
     for sql_table, sql in sqls.items():
       print(f'Using the following sql to run the query: {sql}')
       cursor.execute(sql)
       etl_results[sql_table] = as_pandas(cursor=cursor)
+      # print(etl_results.get("mistplayetl.users"))
     return etl_results
 
-if __name__ == "__main__":
-    path = "/home/maycon/dev/airflow/airflow-submodule/templates/{{cookiecutter.monitoring_name}}/config.json"
-    configs = load_config_file_to_dict(path)
-    result = entrypoint(configs=configs)
-    print(result)
+# if __name__ == "__main__":
+#     path = "/home/maycon/dev/airflow/airflow-submodule/templates/{{cookiecutter.monitoring_name}}/config.json"
+#     configs = load_config_file_to_dict(path)
+#     result = entrypoint(configs=configs)
+#     print(result)
