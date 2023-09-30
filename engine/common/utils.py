3d0c99cdfcd730815b16520b06733961090e3648
Maycon Carlete <maycon.carlete@gmail.com>
2023-09-30 11:44:15 -0400

    remove refactored file

--- engine/common/utils.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_config_file_to_dict(path:str) -> dict:
-    with open(path, "r") as config_file:
-        json_as_string = config_file.read()
-        return json.loads(json_as_string)
-
-def save_etl_to_csv(filename: str) -> None:
-    pass
