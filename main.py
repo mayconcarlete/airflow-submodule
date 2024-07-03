9d57ddb4b87d63942d79ae38feb4c225f200757a
Maycon Carlete <maycon.carlete@gmail.com>
2024-07-02 21:17:26 -0400

    feat: wip

--- main.py
+++ main.py
@@ -4,23 +4,60 @@ from engine.monitoring_factory.monitoring_factory import create_monitoring
 from engine.dynamic_import.main import load_function_to_memory
 from engine.utils.utils import parse_config_to_dict
 import pandas as pd
-# class Menu:
-#     def __init__(self) -> None:
-#         self.options = ["Create Monitoring", "Run Moniroting"]
-#         self.options_two = ["locally", "live"]
+from simple_term_menu import TerminalMenu
 
-#     def print_menu(self) -> int:
-#         terminal_menu = TerminalMenu(self.options)
+def hello():
+    print("hello")
 
-#         terminal_sub_menu = TerminalMenu(self.options_two)
-#         menu_entry_index = terminal_menu.show()
+def test_1():
+    print("test_1")
+def final():
+    print("final")
 
-#         if menu_entry_index == 0:
-#             create_monitoring()
-#         return menu_entry_index
+def get_option(options: dict) -> callable:
+    menu_names = [key for key in options.keys()]
+    menu = TerminalMenu(menu_names)
+    option_picked_as_number = menu.show()
+    option_picked = menu_names[option_picked_as_number]
+    dict_value = options[option_picked]
+    option_picked_type = type(dict_value) # checks if the option picked is a function or a dict(sub menu).
+    
+    while(option_picked_type == dict):
+        menu_names = [key for key in dict_value.keys()]
+        menu = TerminalMenu(menu_names)
+        option_picked_as_number = menu.show()
+        option_picked = menu_names[option_picked_as_number]
+        dict_value = dict_value[option_picked]
+        option_picked_type = type(dict_value) # checks if the option picked is a function or a dict(sub menu).
 
+    return dict_value
+    
 
 if __name__ == "__main__":
+    main_menu_options = {
+        "Create New Monitoring": test_1,
+        "Run Existing Monitoring": hello,
+        "c": {
+            "d":{
+                "e": final
+            }
+        }
+    }
+    fn = get_option(main_menu_options)
+    fn()
+
+    
+
+
+
+
+
+
+
+
+
+
+
     # menu = Menu()
     # result = menu.run()
     # # menu.print_menu()
@@ -36,42 +73,42 @@ if __name__ == "__main__":
     # monitorings[monitoring_name].entrypoint(**config)
 
 
-    data = {
-    "calories": [420, 380, 390],
-    "duration": [50, 40, None]
-    }
+    # data = {
+    # "calories": [420, 380, 390],
+    # "duration": [50, 40, None]
+    # }
 
-    data2 = {
-        "calories3": [2420, 2380, 2390],
-        "duration3": [250, 240, 245]
-    }
+    # data2 = {
+    #     "calories3": [2420, 2380, 2390],
+    #     "duration3": [250, 240, 245]
+    # }
 
-    dataframe = pd.DataFrame(data)
-    dataframe2 = pd.DataFrame(data2)
+    # dataframe = pd.DataFrame(data)
+    # dataframe2 = pd.DataFrame(data2)
 
-    dicta = dataframe.to_dict()
-    results = {
-        "users": dataframe,
-        "ufh": dataframe2
-    }
+    # dicta = dataframe.to_dict()
+    # results = {
+    #     "users": dataframe,
+    #     "ufh": dataframe2
+    # }
 
-    data_to_dict = {}
+    # data_to_dict = {}
 
-    for key, values in results.items():
-        # data_to_dict[key] = values.to_dict()
-        values.to_csv(f'{key}.csv')
+    # for key, values in results.items():
+    #     # data_to_dict[key] = values.to_dict()
+    #     values.to_csv(f'{key}.csv')
 
-    # for key, value in data_to_dict.items():
-        # print(key, value)
+    # # for key, value in data_to_dict.items():
+    #     # print(key, value)
 
-    # df_all = pd.DataFrame(data_to_dict)
-    # df_all.to_csv("data.csv")
+    # # df_all = pd.DataFrame(data_to_dict)
+    # # df_all.to_csv("data.csv")
 
-    # load_df = pd.read_csv("data.csv", index_col=0)
+    # # load_df = pd.read_csv("data.csv", index_col=0)
 
-    # new_dict = load_df.to_dict()
-    # parseado = {}
-    # for key, values in new_dict.items():
-    #     parseado[key] = pd.DataFrame(values, index=[0])
+    # # new_dict = load_df.to_dict()
+    # # parseado = {}
+    # # for key, values in new_dict.items():
+    # #     parseado[key] = pd.DataFrame(values, index=[0])
 
-    # print(parseado["users"])
+    # # print(parseado["users"])
