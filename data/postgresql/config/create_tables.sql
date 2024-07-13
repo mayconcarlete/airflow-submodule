9158af5378da5435ca15f8e9be883f724e795ecf
Maycon Carlete <maycon.carlete@gmail.com>
2024-07-12 23:14:19 -0400

    feat: add new seeds

--- data/postgresql/config/create_tables.sql
+++ data/postgresql/config/create_tables.sql
@@ -9,4 +9,14 @@ CREATE TABLE IF NOT EXISTS users (
 	year INTEGER,
 	created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
-	);
+);
+
+CREATE TABLE IF NOT EXISTS products (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	name VARCHAR(255),
+	price INTEGER,
+	image TEXT,
+	buy_date TIMESTAMPTZ,
+	created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+	updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
